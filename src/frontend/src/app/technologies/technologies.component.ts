954ce63068de04a6b6a891cad394f5802486ffee
Alina Meyer <alina.eichmatt@gmail.com>
2024-02-19 08:02:03 +0100

    Display all published technologies sorted

--- src/frontend/src/app/technologies/technologies.component.ts
+++ src/frontend/src/app/technologies/technologies.component.ts
@@ -1,21 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 import { Technology, TechnologyService } from '../services/technology.service';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-technologies',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './technologies.component.html',
   styleUrl: './technologies.component.scss',
   providers: [TechnologyService]
 })
 export class TechnologiesComponent {
   public publishedTechnologies$: Observable<Technology[]>
+  displayedColumns: string[] = ['name', 'category', 'ring', 'description'];
 
+  //order by
+  // filter
+  //sort
   constructor(private techService: TechnologyService) {
-    this.publishedTechnologies$ = techService.getAllPublishedTechnologies();
+    this.publishedTechnologies$ = techService.getAllPublishedTechnologies().pipe(tap(techs => {
+      techs.sort(
+        (a: Technology, b: Technology) => {
+          let order = a.category.localeCompare(b.category);
+          if (order == 0) { order = a.ring.localeCompare(b.ring); };
+          return order;
+        }
+      )
+    }));
   }
 
 
